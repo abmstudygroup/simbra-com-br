a773b9e4901324e5ac7b4b791e037dfe8e63958c
Daniel Vartanian <danielvartan@proton.me>
2025-10-19 03:43:07 -0300

    Fix Open Graph image

--- R/_post-render.R
+++ R/_post-render.R
@@ -3,14 +3,43 @@
 library(beepr)
 library(fs)
 library(here)
+library(readr)
+library(stringr)
 
 # Add `og-image.png` to `./docs/images` -----
 
-# file <- here("docs", "images", "og-image.png")
+file <- here("docs", "images", "og-image.png")
 
-# if (!file_exists(file)) {
-#   file_copy(here("images", "og-image.png"), file, overwrite = TRUE)
-# }
+if (!file_exists(file)) {
+  file_copy(here("images", "og-image.png"), file, overwrite = TRUE)
+}
+
+# Remove *About* Template Open Graph Meta Tag -----
+
+files <-
+  here("docs") |>
+  dir_ls(type = "file", glob = "*.html")
+
+for (i in files) {
+  if (file_exists(i)) {
+      data_i <- i |> read_lines()
+
+      data_i <-
+        data_i |>
+        magrittr::extract(
+          data_i |>
+            stringr::str_detect(
+              pattern = paste0(
+                "https://abmstudygroup.github.io/simbra-com-br/images/",
+                "simbra-symbol-circular.svg"
+              ),
+              negate = TRUE
+            )
+        )
+
+    data_i |>  write_lines(i)
+  }
+}
 
 # Check If the Script Ran Successfully -----
 
